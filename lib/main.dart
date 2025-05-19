173d2bea1ed66f3bcde4454dc080a3f06d2c2248
Senior Mobile app | Web | API Developer <techableteam@gmail.com>
2025-05-19 18:40:50 -0400

    local notification config in ios

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,18 @@ final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 
 Future<void> initializeNotifications() async {
   const AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('@mipmap/ic_launcher');  // Replace with your app's icon.
+      AndroidInitializationSettings('@mipmap/ic_launcher'); 
+      
+  const DarwinInitializationSettings initializationSettingsIOS =
+      DarwinInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+  );
 
   const InitializationSettings initializationSettings = InitializationSettings(
     android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS
   );
 
   await flutterLocalNotificationsPlugin.initialize(initializationSettings);
